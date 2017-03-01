7c20eb92b03d3ac9c880cffea75f191083e44855
Anthony Pepe [DEV] <apepe@enotes.com>
2017-03-01 22:48:07 +0000

    Added gbs

--- work-aliases.sh
+++ work-aliases.sh
@@ -1,10 +1,10 @@
 alias gbd='gulp build:dev'
 alias gby='gulp build:styles'
 alias lsa='ls -1'
-findw() {
+alias findswap='find . -name "*.sw*"'
+function findw() {
     find . -name "*$1*"
 }
-alias findswap='find . -name "*.sw*"'
-gbs() {
+function gbs() {
     gulp build:scripts --module="$1"
 }
